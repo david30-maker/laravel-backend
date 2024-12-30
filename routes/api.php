414ee1e470c75e9b99c8bf90f36dbdf0311bfca3
david30-maker <davidokpah396@gmail.com>
2024-12-30 19:08:57 +0100

    create authentication

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
